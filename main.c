da2696976de3b452b01c1ff94cb0a7d8e4d35da8
nsyncw <69456282+nsyncw@users.noreply.github.com>
2022-07-22 18:46:10 +0800

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main(argc, argv)
 char *argv[];
 
 {
+	
     PreprocData *pd = walloc(1,sizeof(PreprocData));
     pd->P = 30; // P% percent of total passes in mode 1, the remaining in mode 2
     pd->total = 50; //number of total passes
@@ -14,9 +15,9 @@ char *argv[];
 
     initPreprocData(pd); //init data structure
 
-    preProc(pd); // preproc by traversing the graph for pd->total times
+    preProc(pd); // preproc by traversing the graph for pd->total times，the Lines 1-6 in Algorithm 1 of the paper
 
-    calcuRandomPairs(1000,pd); // randomly choose 100 node pairs and calcu their min-cut and output
+    calcuRandomPairs(1000,pd); // randomly choose 100 node pairs and calcu their min-cut and output. For each pair, it is the logics in Lines 7-10 in Algorithm 1.
   
     exit(0);
 
